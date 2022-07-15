843c77843cb2f1dcd165c1cfc3bb1102c2c20456
Marzieh Valizadeh <sureh1937@gmail.com>
2022-07-15 16:12:26 +0430

    Update the form styles

--- components/SocialForm.tsx
+++ components/SocialForm.tsx
@@ -36,10 +36,15 @@ const optionList = [
     label: "linkedin",
   },
 ];
+interface Social {
+  id: number;
+  type?: string;
+  link?: string;
+}
 const SocialForm: React.FunctionComponent<{
   setIsOpen: Dispatch<SetStateAction<boolean>>;
-  setSocial: Dispatch<SetStateAction<object>>;
-  social: {};
+  setSocial: Dispatch<SetStateAction<Social | null>>;
+  social: Social | null;
   submitHandler: () => void;
 }> = ({ setIsOpen, setSocial, social, submitHandler }) => {
   const [option, setOption] = useState("");
@@ -60,54 +65,52 @@ const SocialForm: React.FunctionComponent<{
     submitHandler();
     setOption("");
     setType("");
-    setSocial({});
+    setSocial(null);
   };
   return (
-    <Paper elevation={2}>
-      <form className={formStyles.socialForm}>
-        <p className={formStyles.title}>Add social</p>
-        <Grid container spacing={1}>
-          <Grid item xs={12} sm={4}>
-            <FormControl fullWidth>
-              <InputLabel id="label">Link</InputLabel>
-              <Select
-                labelId="label"
-                id="select"
-                value={option}
-                name="link"
-                label="Link"
-                onChange={(e) => changeLinkHandler(e)}
-              >
-                {optionList.map((option) => (
-                  <MenuItem key={option.value} value={option.value}>
-                    {option.label}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} sm={8}>
-            <TextField
-              fullWidth
-              id="outlined-basic"
-              label="type"
-              variant="outlined"
-              onChange={(e) => changeTypeHandler(e)}
-              name="type"
+    <form className={formStyles.socialForm}>
+      <p className={formStyles.title}>Add social</p>
+      <Grid container spacing={1}>
+        <Grid item xs={12} sm={4}>
+          <FormControl fullWidth>
+            <InputLabel id="label">Type</InputLabel>
+            <Select
+              labelId="label"
+              id="select"
               value={type}
-            />
-          </Grid>
+              name="type"
+              label="Type"
+              onChange={(e) => changeTypeHandler(e)}
+            >
+              {optionList.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
         </Grid>
-        <div className={formStyles.formBottom}>
-          <Button variant="contained" onClick={clickHandler}>
-            Add
-          </Button>
-          <Button variant="outlined" onClick={() => setIsOpen(false)}>
-            cancel
-          </Button>
-        </div>
-      </form>
-    </Paper>
+        <Grid item xs={12} sm={8}>
+          <TextField
+            fullWidth
+            id="outlined-basic"
+            label="Link"
+            variant="outlined"
+            onChange={(e) => changeLinkHandler(e)}
+            name="link"
+            value={option}
+          />
+        </Grid>
+      </Grid>
+      <div className={formStyles.formBottom}>
+        <Button variant="contained" onClick={clickHandler}>
+          Add
+        </Button>
+        <Button variant="outlined" onClick={() => setIsOpen(false)}>
+          cancel
+        </Button>
+      </div>
+    </form>
   );
 };
 
