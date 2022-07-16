e370e7b1b4486e6d75646528957e497acf35b3cb
Marzieh Valizadeh <sureh1937@gmail.com>
2022-07-16 16:15:11 +0430

    feat: Get and Post the socials using local API

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,12 @@ import { Button, Paper } from "@mui/material";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 import AddIcon from "@mui/icons-material/Add";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SocialForm from "../components/SocialForm";
 import SocialItem from "../components/SocialItem";
 import { Trans, useTranslation } from "react-i18next";
 import { Social } from "../ts/interfaces";
+import { addNewData, getAllData } from "../services/CRUDServices";
 
 const Home: NextPage = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -14,6 +15,7 @@ const Home: NextPage = () => {
   const [social, setSocial] = useState<Social | null>(null);
   const submitHandler = () => {
     setSocials([...socials, { ...social, id: Date.now() }]);
+    addCommentHandler(social);
   };
   const onDelete = (id: number) => {
     const cloneSocials = [...socials];
@@ -21,7 +23,14 @@ const Home: NextPage = () => {
     setSocials(filteredsocials);
   };
   const { t } = useTranslation();
-
+  useEffect(() => {
+    getAllData().then((res) => setSocials(res.data));
+  }, []);
+  const addCommentHandler = async (social: Social) => {
+    try {
+      await addNewData(social);
+    } catch (error) {}
+  };
   return (
     <Paper elevation={2} className={styles.mainBox}>
       <p className={styles.title}> {t("title")}</p>
